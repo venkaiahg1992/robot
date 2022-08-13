406daee526991153bc86e4e74442fef32d9bb15b
venkaiahg1992 <67815613+venkaiahg1992@users.noreply.github.com>
2022-08-13 22:12:17 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,2 +1,4 @@
 python -m robot test.txt
-copy log.html report.html output.xml  C:\Venky\Robot_result\
+copy log.html  C:\Venky\Robot_result\
+copy report.html C:\Venky\Robot_result\
+copy output.xml C:\Venky\Robot_result\
